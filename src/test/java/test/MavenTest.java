a2c569e0ab373c1977dbf1825be1e7f57a718413
Preethi Elangovan <Preethi Elangovan@LAPTOP-3UOT6CPJ>
2018-07-17 20:00:18 +0530

    First commit

---
+++ src/test/java/test/MavenTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class MavenTest 
+{
+	@Test
+	public void testA()
+	{
+		Reporter.log("hiii....", true);
+	}
+
+}
